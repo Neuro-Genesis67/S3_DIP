23a0170b29932d33b5805b176c02f873ab496966
Tom <neuro.genesis67@gmail.com>
2019-01-31 21:50:43 +0100

    PC

---
+++ 3. Semester/L2 - Funktioner, objekter og arrays/Temp/Objekter.js
@@ -0,0 +1,17 @@
+// let objekt1 = {navn: "objektets navn", "værdi": 42};
+// let objekt2 = {id: 47, "My_value_is_a_list": [42, 12, 32]};
+// let objekt3 = {id: "name", "b": 1, "c": 2, "d": 3, "e": 42};
+//
+// console.log("objekt1's navn property: " + objekt1.navn);
+// console.log("objekt1's værdi: " + objekt1.værdi);
+//
+// console.log("objekt2's navn property: " + objekt2.id)
+// console.log("objekt2's værdi: " + objekt2.My_value_is_a_list);
+//
+// console.log(objekt3.e);
+
+
+// x.log + tab -----> console.log(x);
+
+
+
