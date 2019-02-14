e069ac0333bdac395e45581b3712e20341b170cf
Tom <neuro.genesis67@gmail.com>
2019-02-14 12:33:14 +0100

    Lap

--- 3. Semester/L1 - Introduktion/Materiale/typeof.js
+++
@@ -1,6 +0,0 @@
-// typeof.js
-console.log(typeof 123); // => number
-console.log(typeof 'abc'); // => string
-console.log(typeof true); // => boolean
-console.log(typeof null); // => object
-console.log(typeof undefined); // => undefined
--- 3. Semester/L2 - Funktioner, objekter og arrays/2.5/Answer.js
+++
@@ -1,6 +0,0 @@
-let theString = "Kinbaku var en fantastisk oplevelse. Jeg så verden fra et helt nyt perspektiv og det var en meget belønnende aktivitet";
-let mapOfWords = theString.split(" ");
-
-console.log("The string: \n", theString, "\n");
-console.log("The mapOfWords object: \n", mapOfWords, "\n");
-console.log("Words in the map: \n", +mapOfWords.length, "\n");
