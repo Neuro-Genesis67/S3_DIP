b8b8765b5c64652e73f306a4b83629047c142374
Tom Steinmeier <neuro.genesis67@gmail.com>
2019-05-28 18:58:52 +0200

    lp

---
+++ 3. Semester/L16 - Templating og Arkitektur/hibscus/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs').promises;
+
+const app = express();
+app.set('view engine', 'hbs');
+app.set('views', 'templates');
+
+// console.log(__dirname); = C:\Program Files (x86)\Eclipse\Github\S3_DIP\3. Semester\L16 - Templating og Arkitektur\hibscus
+console.log(path.join(__dirname, '\\filer\\'));
+
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index', {
+        "filer": fs.readdir(__dirname + '/filer').then(filer => {
+
+        })
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {console.log(`Listening on port ${port}`)});
